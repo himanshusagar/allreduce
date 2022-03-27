a45f01e841441e25e1d422f6a61c3c045a99f60e
himanshusagar <himanshusagar@protonmail.com>
2022-03-27 17:01:55 -0500

    Added ignore and sh

--- recursive/run.sh
+++ recursive/run.sh
@@ -4,11 +4,13 @@
 # Assumes worker nodes have hostname node1, node2, ... node15
 # Assumes you can ssh from node0 to all the other nodes.
 
-pytbon3 main.py --master-ip 10.10.1.1 --num-nodes 16 --rank 0
+#python3 main.py --master-ip 10.10.1.1 --num-nodes 16 --rank 0
 
-for i in `seq 1 15`
+for i in `seq 0 15`
 do
         RANK=$i
         echo "Staring rank $RANK"
-        ssh -f node$i "nohup python3 main.py --master-ip 10.10.1.1 --num-nodes 16 --rank $RANK"
+        ssh -f a$i "nohup python3 /users/hsagar/allreduce/recursive/main.py --master-ip 10.10.1.1 --num-nodes 16 --rank $RANK"
 done
+
+#python3 main.py --master-ip 10.10.1.1 --num-nodes 16 --rank 0
