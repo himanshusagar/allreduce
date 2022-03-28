54ca77e552d9cbf0c677af438975bc1fa96887e6
himanshusagar <himanshusagar@protonmail.com>
2022-03-27 20:37:21 -0500

    Port changed.

--- recursive/main.py
+++ recursive/main.py
@@ -17,7 +17,7 @@ class RecursiveAllReduce:
 
     def init_process(self, master_ip, rank, world_size):
         dist.init_process_group(backend="gloo",
-                                init_method="tcp://" + master_ip + ":6585",
+                                init_method="tcp://" + master_ip + ":6588",
                                 rank=rank,
                                 world_size=world_size)
         self.my_rank = dist.get_rank()
