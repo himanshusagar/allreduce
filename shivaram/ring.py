32c743c235da1499fc4c85fb44fc639a04e94d53
AditiSingh97 <aditi.singh@iitgn.ac.in>
2022-04-01 17:57:33 -0500

    Added timers to ring.py

--- shivaram/ring.py
+++ shivaram/ring.py
@@ -15,15 +15,13 @@ def init_process(master_ip, rank, world_size):
                             world_size=world_size)
 
 
-def main():
+def main(tensor_size):
     # Create a random tensor
-    t = torch.rand(TENSOR_SIZE)
+    t = torch.rand(tensor_size)
     # indices to send and receive from
     me = dist.get_rank()
-    print("On node " + str(me) + " tensor is : " + str(t))
     world_size = dist.get_world_size()
-    print(world_size)
-    comm_size = int(TENSOR_SIZE/world_size)
+    comm_size = int(tensor_size/world_size)
 
     prev = me-1
     if(prev < 0):
@@ -34,17 +32,20 @@ def main():
 
     curi = me
     for i in range(0, world_size):
-        print("For iter : " + str(i) + ", on node " + str(me) + " tensor is : " + str(t))
         if(me%2 == 0):
-            print("here " + str(me))
             send_buf = torch.zeros(comm_size)
             for idx in range(0,comm_size):
                 send_buf[idx]=t[curi*comm_size + idx]
+            s=time.time()
             dist.send(send_buf, dst=_next_)
-            print("Finished send to ", _next_);
+            e=time.time()
+            print("Finished send to ", _next_, " in ", e - s, " seconds ")
+
             recv_buf = torch.zeros(comm_size)
+            s=time.time()
             dist.recv(recv_buf, src=prev)
-            print("Finished recv from ", prev)
+            e=time.time()
+            print("Finished recv from ", prev, " in ", e - s, " seconds ")
             k = 0
             curi = curi-1
             if(curi < 0):
@@ -53,10 +54,11 @@ def main():
                 t[j] = t[j]+recv_buf[k]
                 k=k+1
         else:
-            print("here " + str(me))
             recv_buf = torch.zeros(comm_size)
+            s=time.time()
             dist.recv(recv_buf, src=prev)
-            print("Finished recv from ", prev)
+            e=time.time()
+            print("Finished recv from ", prev, " in ", e - s, " seconds ")
             k = 0
             curi = curi-1
             if(curi < 0):
@@ -71,7 +73,10 @@ def main():
             send_buf = torch.zeros(comm_size)
             for idx in range(0,comm_size):
                 send_buf[idx]=t[curi*comm_size + idx]
+            s=time.time()
             dist.send(send_buf, dst=_next_)
+            e=time.time()
+            print("Finished send to ", _next_, " in ", e - s, " seconds ")
             curi = curi-1
             if(curi < 0):
                 curi = world_size-1
@@ -81,12 +86,13 @@ def main():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--tensor-size", "-m", required=False, type=int, default=1024)
     parser.add_argument("--master-ip", "-m", required=True, type=str)
-    parser.add_argument("--num-nodes", "-n", required=True, type=int)
+    parser.add_argument("--num-nodes", "-n", required=False, type=int, default=16)
     parser.add_argument("--rank", "-r", required=True, type=int)
 
     args = parser.parse_args()
     init_process(master_ip=args.master_ip,
                  rank=args.rank,
                  world_size=args.num_nodes)
-    main()
+    main(tensor_size=args.tensor_size)
