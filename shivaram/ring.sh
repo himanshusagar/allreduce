9fdd409528b141cc0b2d43de40a4b50372fb0933
AditiSingh97 <aditi.singh@iitgn.ac.in>
2022-04-01 17:20:38 -0500

    More debug statements....

--- shivaram/ring.sh
+++ shivaram/ring.sh
@@ -8,11 +8,11 @@
 
 parallel-ssh -i -h ~/followers "cd allreduce && git pull"
 
-python3 ~/allreduce/shivaram/ring.py --master-ip 10.10.1.1 --num-nodes 16 --rank 0 &
+python3 ~/allreduce/shivaram/ring.py --master-ip 10.10.1.1 --num-nodes 2 --rank 0 &
 
-for i in `seq 1 15`
+for i in `seq 1`
 do
         RANK=$i
         echo "Starting rank $RANK"
-        ssh -f a$i "nohup python3 /users/hsagar/allreduce/shivaram/ring.py --master-ip 10.10.1.1 --num-nodes 16 --rank $RANK"
+        ssh -f a$i "nohup python3 /users/hsagar/allreduce/shivaram/ring.py --master-ip 10.10.1.1 --num-nodes 2 --rank $RANK"
 done
