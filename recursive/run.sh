2c5cb2cf1129bb685610b465fdb0796e9b51321d
himanshusagar <himanshusagar@protonmail.com>
2022-04-01 17:57:07 -0500

    More sizes 2

--- recursive/run.sh
+++ recursive/run.sh
@@ -5,7 +5,7 @@
 # Assumes you can ssh from node0 to all the other nodes.
 
 parallel-ssh -i -h ~/followers "cd dev && cd allreduce && git pull"
-T_SIZE=4
+T_SIZE=8
 W_SIZE=4
 
 END_LOOP=$(expr $W_SIZE - 1)
