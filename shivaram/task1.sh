423ce786bdf9891103f7516753f05c64e9ba271c
AditiSingh97 <aditi.singh@iitgn.ac.in>
2022-04-02 09:26:50 -0500

    Adding log for send times with diff tensor sizes in ring reduce, testing world size 2

--- shivaram/task1.sh
+++ shivaram/task1.sh
@@ -8,6 +8,7 @@
 
 parallel-ssh -i -h ~/followers "cd allreduce && git pull"
 
+<<com
 KB=1024
 #4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072
 for j in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072
@@ -22,16 +23,17 @@ do
 
 	wait
 done
+com
 
 
-<<com
 KB=1024
 k=10
 T_SIZE=$((k*KB))
 T_SIZE=$((T_SIZE*KB))
 ssh -f a1 "nohup python3 /users/hsagar/allreduce/shivaram/ring.py -t $T_SIZE --master-ip 10.10.1.1 --num-nodes 2 --rank 1"
-python3 ~/allreduce/shivaram/ring.py -t $T_SIZE --master-ip 10.10.1.1 --num-nodes 2 --rank 0 &
+python3 ~/allreduce/shivaram/ring.py -t $T_SIZE --master-ip 10.10.1.1 --num-nodes 2 --rank 0
 
+<<com
 k=10
 T_SIZE=$((k*KB))
 T_SIZE=$((T_SIZE*KB))
@@ -43,7 +45,7 @@ do
 		RANK=$i
         	ssh -f a$i "nohup python3 /users/hsagar/allreduce/shivaram/ring.py -t $T_SIZE --master-ip 10.10.1.1 --num-nodes $k --rank $RANK"
 	done
-	python3 ~/allreduce/shivaram/ring.py -t $T_SIZE --master-ip 10.10.1.1 --num-nodes $k --rank 0 &
+	python3 ~/allreduce/shivaram/ring.py -t $T_SIZE --master-ip 10.10.1.1 --num-nodes $k --rank 0
 
 	wait
 done
