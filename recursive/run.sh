63630638fad783d8151625a6f8adb15805eef706
himanshusagar <himanshusagar@protonmail.com>
2022-04-01 20:20:46 -0500

    let's try this

--- recursive/run.sh
+++ recursive/run.sh
@@ -14,13 +14,16 @@ W_SIZE=$2
 END_LOOP=$(($W_SIZE-1))
 
 echo "inside run_func $T_SIZE $W_SIZE $END_LOOP"
-python3 ~/dev/allreduce/recursive/main.py --master-ip 10.10.1.1 --num-nodes $W_SIZE --rank 0 --tensor-size $T_SIZE &
+
 for i in `seq 1 $END_LOOP`
 do
         RANK=$i
         echo "Staring rank $RANK $WORLD_SIZE $T_SIZE"
         ssh -f a$i "nohup python3 ~/dev/allreduce/recursive/main.py --master-ip 10.10.1.1 --num-nodes $W_SIZE --rank $RANK --tensor-size $T_SIZE"
 done
+
+python3 ~/dev/allreduce/recursive/main.py --master-ip 10.10.1.1 --num-nodes $W_SIZE --rank 0 --tensor-size $T_SIZE
+
 }
 
 echo "Start of script..."
