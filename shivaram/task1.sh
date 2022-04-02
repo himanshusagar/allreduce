2cf4484646f919ade39ae17ab47a27a534db7ba4
AditiSingh97 <aditi.singh@iitgn.ac.in>
2022-04-02 01:26:43 -0500

    Debug....

--- shivaram/task1.sh
+++ shivaram/task1.sh
@@ -9,7 +9,7 @@
 parallel-ssh -i -h ~/followers "cd allreduce && git pull"
 KB=1024
 #4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072
-for j in 1 2 
+for j in 1
 do
 	T_SIZE=$((j*KB))
 	echo $T_SIZE
