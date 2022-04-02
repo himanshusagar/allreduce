26f57a5c0a006dcdd78c5f76aed2e6c414aef0d2
himanshusagar <himanshusagar@protonmail.com>
2022-04-01 20:44:14 -0500

    Try PORT

--- recursive/run.sh
+++ recursive/run.sh
@@ -11,18 +11,19 @@ run_func()
 
 T_SIZE=$1
 W_SIZE=$2
+PORT_VAL=$3
 END_LOOP=$(($W_SIZE-1))
 
-#echo "inside run_func $T_SIZE $W_SIZE $END_LOOP"
+echo "inside run_func $T_SIZE $W_SIZE $END_LOOP"
 
 for i in `seq 1 $END_LOOP`
 do
         RANK=$i
         #echo "Staring rank $RANK $WORLD_SIZE $T_SIZE"
-        ssh -f a$i "nohup python3 ~/dev/allreduce/recursive/main.py --master-ip 10.10.1.1 --num-nodes $W_SIZE --rank $RANK --tensor-size $T_SIZE"
+        #ssh -f a$i "nohup python3 ~/dev/allreduce/recursive/main.py --master-ip 10.10.1.1 --num-nodes $W_SIZE --rank $RANK --tensor-size $T_SIZE --port $PORT_VAL"
 done
 
-python3 ~/dev/allreduce/recursive/main.py --master-ip 10.10.1.1 --num-nodes $W_SIZE --rank 0 --tensor-size $T_SIZE
+#python3 ~/dev/allreduce/recursive/main.py --master-ip 10.10.1.1 --num-nodes $W_SIZE --rank 0 --tensor-size $T_SIZE --port $PORT_VAL &
 
 }
 
@@ -34,7 +35,9 @@ MB=$((KB*KB))
 HundredMB=$(($MB*100))
 
 WORLD_SIZE=16
+PORT_VAL=7581
 
 for tensor_size in `seq $KB $STEPSIZE $HundredMB`; do
-      run_func $tensor_size $WORLD_SIZE
+      run_func $tensor_size $WORLD_SIZE $PORT_VAL
+      PORT_VAL=$((PORT_VAL+2))
 done
