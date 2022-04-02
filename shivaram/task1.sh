eb5116045b00c0a55c2f7f58978e1bcffa96351c
AditiSingh97 <aditi.singh@iitgn.ac.in>
2022-04-02 01:42:57 -0500

    Testing different world sizes

--- shivaram/task1.sh
+++ shivaram/task1.sh
@@ -7,12 +7,13 @@
 #python3 ring.py --master-ip 10.10.1.1 --num-nodes 16 --rank 0 
 
 parallel-ssh -i -h ~/followers "cd allreduce && git pull"
+
+:'
 KB=1024
 #4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072
-for j in 1 2
+for j in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072
 do
 	T_SIZE=$((j*KB))
-	echo $T_SIZE
 	for i in `seq 1 15`
 	do
 		RANK=$i
@@ -22,3 +23,32 @@ do
 
 	wait
 done
+'
+
+
+KB=1024
+k=10
+T_SIZE=$((k*KB))
+T_SIZE=$((T_SIZE*KB))
+ssh -f a1 "nohup python3 /users/hsagar/allreduce/shivaram/ring.py -t $T_SIZE --master-ip 10.10.1.1 --num-nodes 2 --rank 1"
+python3 ~/allreduce/shivaram/ring.py -t $T_SIZE --master-ip 10.10.1.1 --num-nodes 2 --rank 0 &
+wait
+
+:'
+k=10
+T_SIZE=$((k*KB))
+T_SIZE=$((T_SIZE*KB))
+for j in 4 8 16
+do
+	j_lim=$((j-1))
+	for i in `seq 1 $j_lim`
+	do
+		RANK=$i
+        	ssh -f a$i "nohup python3 /users/hsagar/allreduce/shivaram/ring.py -t $T_SIZE --master-ip 10.10.1.1 --num-nodes $k --rank $RANK"
+	done
+	python3 ~/allreduce/shivaram/ring.py -t $T_SIZE --master-ip 10.10.1.1 --num-nodes $k --rank 0 &
+
+	wait
+done
+'
+
