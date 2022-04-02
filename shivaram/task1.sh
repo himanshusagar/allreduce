48f590a8da3cffec27f1ae57052e16bf28d9a184
AditiSingh97 <aditi.singh@iitgn.ac.in>
2022-04-02 01:24:43 -0500

    Debug....

--- shivaram/task1.sh
+++ shivaram/task1.sh
@@ -11,7 +11,7 @@ KB=1024
 #4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072
 for j in 1 2 
 do
-	T_SIZE=$(${j}*${KB})
+	T_SIZE=$((j*KB))
 	echo $T_SIZE
 	for i in `seq 1 15`
 	do
