466b58fd0926671c43767847b6b549f1c5e8202a
himanshusagar <himanshusagar@protonmail.com>
2022-04-01 20:17:54 -0500

    Shell Script

--- recursive/run.sh
+++ recursive/run.sh
@@ -11,25 +11,25 @@ run_func()
 
 T_SIZE=$1
 W_SIZE=$2
-END_LOOP=$(expr $W_SIZE - 1)
-echo "run_func  $W_SIZE $T_SIZE"
-#python3 ~/dev/allreduce/recursive/main.py --master-ip 10.10.1.1 --num-nodes $W_SIZE --rank 0 --tensor-size $T_SIZE &
+END_LOOP=$(($W_SIZE-1))
 
+echo "inside run_func $T_SIZE $W_SIZE $END_LOOP"
+python3 ~/dev/allreduce/recursive/main.py --master-ip 10.10.1.1 --num-nodes $W_SIZE --rank 0 --tensor-size $T_SIZE &
 for i in `seq 1 $END_LOOP`
 do
         RANK=$i
-        #echo "Staring rank $RANK $WORLD_SIZE $T_SIZE"
-        #ssh -f a$i "nohup python3 ~/dev/allreduce/recursive/main.py --master-ip 10.10.1.1 --num-nodes $W_SIZE --rank $RANK --tensor-size $T_SIZE"
+        echo "Staring rank $RANK $WORLD_SIZE $T_SIZE"
+        ssh -f a$i "nohup python3 ~/dev/allreduce/recursive/main.py --master-ip 10.10.1.1 --num-nodes $W_SIZE --rank $RANK --tensor-size $T_SIZE"
 done
 }
 
 echo "Start of script..."
 KB=1024
-MB=$((KB * KB))
-HundredMB=$(expr $MB * 100)
+MB=$((KB*KB))
+HundredMB=$(($MB*100))
 WORLD_SIZE=16
 
-for tensor_size in $(seq $KB $HundredMB $KB)
-do
-  run_func $tensor_size $WORLD_SIZE
+for tensor_size in `seq $KB $KB $KB`; do
+      run_func $tensor_size $WORLD_SIZE
+      break
 done
