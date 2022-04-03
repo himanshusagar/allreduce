be157069fdcd2b8ab6dc111f4915bd1cc186f911
himanshusagar <himanshusagar@protonmail.com>
2022-04-02 19:52:28 -0500

    eq_solver

---
+++ recursive/eq_solver.py
@@ -0,0 +1,31 @@
+
+from sympy import symbols, Eq, solve
+from math import log2
+import numpy as np
+
+#p = 10 # node count
+#n = 1024 #tensor size
+#alpha = constant cost to send each message
+#beta = cost to send/recv each word of message
+#gamma = cost of computation
+
+def getAlphaBeta(p , n):
+    p_1_div_p = (p-1)/p
+    alphaCoeff = 2 * log2(p)
+    gammaCoeff = p_1_div_p * n
+    betaCoeff = gammaCoeff * 2
+    return np.array([alphaCoeff , betaCoeff]);
+
+# defining symbols used in equations
+# or unknown variables
+alpha, beta = symbols('alpha beta' )
+
+
+A = np.array([ getAlphaBeta( 16 , 67108864) ,
+               getAlphaBeta( 16 , 16777216) ])
+
+b = np.array([ 32.2343047 , 49.32134488 ])
+
+print(np.linalg.solve(A, b))
+
+
