eb862ed10f347515795d793f780863bcb3140995
himanshusagar <himanshusagar@protonmail.com>
2022-04-05 16:59:24 -0500

    Same as Init

--- measure/run.sh
+++ measure/run.sh
@@ -4,15 +4,14 @@
 # Assumes worker nodes have hostname node1, node2, ... node15
 # Assumes you can ssh from node0 to all the other nodes.
 
-#python3 main.py --master-ip 10.10.1.1 --num-nodes 16 --rank 0 
-
 parallel-ssh -i -h ~/followers "cd dev && cd allreduce && git pull"
+
+python3 main.py --master-ip 10.10.1.1 --num-nodes 16 --rank 0
 
 for i in `seq 1 15`
 do
         RANK=$i
         echo "Staring rank $RANK"
-        ssh -f a$i "nohup python3 /users/hsagar/allreduce/shivaram/main.py --master-ip 10.10.1.1 --num-nodes 16 --rank $RANK"
+        ssh -f node$i "nohup python3 /users/hsagar/dev/allreduce/measure/main.py --master-ip 10.10.1.1 --num-nodes 16 --rank $RANK"
 done
 
-python3 main.py --master-ip 10.10.1.1 --num-nodes 16 --rank 0 &
